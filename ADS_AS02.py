41d0a63035601fb75aa7a372e812b966e914a18f
Bhagyaw <40148642+Bhagyaw@users.noreply.github.com>
2023-04-05 22:58:04 +0100

    Add files via upload

--- ADS_AS02.py
+++ ADS_AS02.py
@@ -39,8 +39,204 @@ print(data_co2emi_t_cleaned)
 print(data_co2emi_t_cleaned.iloc[1:, :])
 
 # plot the bar chart
-ax = data_co2emi_t_cleaned.iloc[1:, :].plot(kind='bar', stacked=True, colormap='rainbow')
-ax.set_xlabel('Country')
-ax.set_ylabel('CO2 Emission (in KT)')
-ax.set_title('CO2 Emission for Brazil and China from 1990 to 2015')
+chart1 = data_co2emi_t_cleaned.iloc[1:, :].plot(kind='bar', stacked=False, colormap='rainbow')
+chart1.set_xlabel('Year')
+chart1.set_ylabel('CO2 Emission (in KT)')
+chart1.set_title('CO2 Emission from 1990 to 2015')
+chart1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.savefig('CO2_Emission.png')
+plt.show()
+
+
+#Start of main program
+#Reading data from the source file. Here source file type is csv. 
+#Therefore pandas reading funtion must be pd.read_csv
+data_Electricity = pd.read_csv("API_EG.ELC.FOSL.ZS_DS2_en_csv_v2_5358479.csv", header=2, index_col='Country Name', usecols=['Country Name', 'Indicator Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020'])
+
+
+print(data_Electricity)
+#selected_countries = ['Brazil', 'United States', 'China', 'United Kingdom', 'Australia', 'Russian Federation', 'South Africa', 'Canada', 'India', 'Spain']
+
+selected_data_Electricity = data_Electricity.loc[selected_countries]
+print(selected_data_Electricity)
+
+# transpose the dataframe
+data_Electricity_transposed = selected_data_Electricity.transpose()
+
+
+print(data_Electricity_transposed)
+
+data_Electricity_t_cleaned = data_Electricity_transposed.dropna()
+
+# set the year column as an integer
+
+data_Electricity_t_cleaned.iloc[1:, :] = data_Electricity_t_cleaned.iloc[1:, :].astype(int)
+
+print(data_Electricity_t_cleaned)
+
+print(data_Electricity_t_cleaned.iloc[1:, :])
+
+# plot the bar chart
+chart2 = data_Electricity_t_cleaned.iloc[1:, :].plot(kind='bar', stacked=False, colormap='rainbow')
+chart2.set_xlabel('Year')
+chart2.set_ylabel('Electricity production from oil, gas and coal sources (% of total)')
+chart2.set_title('Electricity Generation from 1990 to 2015')
+chart2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.savefig('Electricity Generation.png')
+plt.show()
+
+data_population = pd.read_csv("API_SP.POP.TOTL_DS2_en_csv_v2_5358404.csv", header=2, index_col='Country Name', usecols=['Country Name', 'Indicator Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020'])
+
+selected_data_population = data_population.loc[selected_countries]
+print(selected_data_population)
+
+# transpose the dataframe
+data_population_transposed = selected_data_population.transpose()
+
+print(data_population_transposed)
+
+data_population_t_cleaned = data_population_transposed.dropna()
+
+# set the year column as an integer
+
+data_population_t_cleaned.iloc[1:, :] = data_population_t_cleaned.iloc[1:, :].astype(int)
+
+print(data_population_t_cleaned)
+
+print(data_population_t_cleaned.iloc[1:, :])
+
+plt.figure()
+#For loop to load required data using headers.
+plt.plot(data_population_t_cleaned.iloc[1:, :])
+   
+#labelling x axis and y axis
+plt.xlabel('Year')
+plt.ylabel('Population (% of total)')
+
+#Add legend to explain the each lineplot 
+plt.legend(selected_countries, loc='center left', bbox_to_anchor=(1, 0.5))
+#Add Title to the graph
+plt.title("Population (% of total) from 1990 to 2015")
+# save the plot output as png
+plt.savefig("Population.png")
+#Display the plot
+plt.show()
+
+data_powerconsum = pd.read_csv("API_EG.USE.ELEC.KH.PC_DS2_en_csv_v2_5359189.csv", header=2, index_col='Country Name', usecols=['Country Name', 'Indicator Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020'])
+
+selected_data_powerconsum = data_powerconsum.loc[selected_countries]
+print(selected_data_powerconsum)
+
+# transpose the dataframe
+data_powerconsum_transposed = selected_data_powerconsum.transpose()
+
+
+print(data_powerconsum_transposed)
+
+data_powerconsum_t_cleaned = data_powerconsum_transposed.dropna()
+
+# set the year column as an integer
+
+data_powerconsum_t_cleaned.iloc[1:, :] = data_powerconsum_t_cleaned.iloc[1:, :].astype(int)
+
+print(data_powerconsum_t_cleaned)
+
+print(data_powerconsum_t_cleaned.iloc[1:, :])
+
+plt.figure()
+#For loop to load required data using headers.
+plt.plot(data_powerconsum_t_cleaned.iloc[1:, :])
+   
+#labelling x axis and y axis
+plt.xlabel('Year')
+plt.ylabel('Electric  Power consumption (kWh per capita)')
+
+#Add legend to explain the each lineplot 
+plt.legend(selected_countries, loc='center left', bbox_to_anchor=(1, 0.5))
+#Add Title to the graph
+plt.title("Electric  Power consumption from 1990 to 2015")
+# save the plot output as png
+plt.savefig("Electric  Power consumption.png")
+#Display the plot
+plt.show()
+
+
+
+
+data_nuclearpower = pd.read_csv("API_EG.ELC.NUCL.ZS_DS2_en_csv_v2_5362829.csv", header=2, index_col='Country Name', usecols=['Country Name', 'Indicator Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020'])
+
+selected_data_nuclearpower = data_nuclearpower.loc[selected_countries]
+print(selected_data_nuclearpower)
+
+# transpose the dataframe
+data_nuclearpower_transposed = selected_data_nuclearpower.transpose()
+
+
+print(data_nuclearpower_transposed)
+
+data_nuclearpower_t_cleaned = data_nuclearpower_transposed.dropna()
+
+# set the year column as an integer
+
+data_nuclearpower_t_cleaned.iloc[1:, :] = data_nuclearpower_t_cleaned.iloc[1:, :].astype(int)
+
+print(data_nuclearpower_t_cleaned)
+
+print(data_nuclearpower_t_cleaned.iloc[1:, :])
+
+plt.figure()
+#For loop to load required data using headers.
+plt.plot(data_nuclearpower_t_cleaned.iloc[1:, :])
+   
+#labelling x axis and y axis
+plt.xlabel('Year')
+plt.ylabel('Electricity production from nuclear sources (% of total)')
+
+#Add legend to explain the each lineplot 
+plt.legend(selected_countries, loc='center left', bbox_to_anchor=(1, 0.5))
+#Add Title to the graph
+plt.title("Electricity production from nuclear sources from 1990 to 2015")
+# save the plot output as png
+plt.savefig("Electricity production from nuclear source.png")
+#Display the plot
+plt.show()
+
+
+
+data_renewable = pd.read_csv("API_EG.ELC.RNWX.KH_DS2_en_csv_v2_5358682.csv", header=2, index_col='Country Name', usecols=['Country Name', 'Indicator Name', '1990', '1995', '2000', '2005', '2010', '2015', '2020'])
+
+selected_data_renewable = data_renewable.loc[selected_countries]
+print(selected_data_renewable)
+
+# transpose the dataframe
+data_renewable_transposed = selected_data_renewable.transpose()
+
+
+print(data_renewable_transposed)
+
+data_renewable_t_cleaned = data_renewable_transposed.dropna()
+
+# set the year column as an integer
+
+#data_renewable_t_cleaned.iloc[1:, :] = data_renewable_t_cleaned.iloc[1:, :].astype(int)
+
+print(data_renewable_t_cleaned)
+
+print(data_renewable_t_cleaned.iloc[1:, :])
+
+plt.figure()
+#For loop to load required data using headers.
+plt.plot(data_renewable_t_cleaned.iloc[1:, :])
+   
+#labelling x axis and y axis
+plt.xlabel('Year')
+plt.ylabel('Electricity production from renewable sources, excluding hydroelectric (kWh)')
+
+#Add legend to explain the each lineplot 
+plt.legend(selected_countries, loc='center left', bbox_to_anchor=(1, 0.5))
+#Add Title to the graph
+plt.title("Electricity production from renewable sources from 1990 to 2015")
+# save the plot output as png
+plt.savefig("Electricity production from renewable sources.png")
+#Display the plot
 plt.show()
